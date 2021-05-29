2c43eb92a005e7cd48b580f468a45de1c298dec6
tilekbai <kukushokk@gmail.com>
2021-05-29 17:18:53 +0600

    Создан login, logout

--- source/project/urls.py
+++ source/project/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gallery.urls', namespace='photo-list')),
+    path('', include('accounts.urls', namespace='account')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
