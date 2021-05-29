44d7e6dae2598af5e9394232c4091165dd051f63
tilekbai <kukushokk@gmail.com>
2021-05-29 11:31:14 +0600

    Созданы модели Photo, Albom

--- source/gallery/admin.py
+++ source/gallery/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Photo, Albom
 
 # Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'caption', 'albom', 'date', 'photo']
+    list_filter = ['author']
+    search_fields = ['author']
+    fields = ['id', 'author', 'caption', 'albom', 'date']
+    readonly_fields = ['id', 'date']
+
+
+class AlbomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'name', 'description', 'date']
+    list_filter = ['author']
+    search_fields = ['author']
+    fields = ['id', 'author', 'name', 'description']
+    readonly_fields = ['id', 'date']
+
+
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Albom, AlbomAdmin)
+
+
--- source/gallery/models.py
+++ source/gallery/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields import BLANK_CHOICE_DASH
 
+User = get_user_model()
 # Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(blank=False)
+    caption = models.CharField(max_length=155, blank=False)
+    date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    albom = models.CharField(max_length=100, blank=True)
+
+
+class Albom(models.Model):
+    name = models.CharField(max_length=155, blank=False)
+    description = models.TextField(max_length=555, blank=True)
+    author = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
